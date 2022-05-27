4c8887e737e6d6ec3d941b2da8781055f7116f2c
Martin Franckie <martinfranckie@Martins-MacBook-Pro.local>
2022-05-27 10:47:21 +0200

    Initial upload. Dummy code for updating power meter until acquisition card has been implemented.

---
+++ mainwindow.py
@@ -0,0 +1,162 @@
+# This Python file uses the following encoding: utf-8
+
+import sys
+import numpy as np
+
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtCore import pyqtSlot, QTimer
+from PyQt6.QtGui import QPixmap
+from PyQt6 import uic
+from scorebar import ScoreBar
+
+STATUS = {"RUNNING": 0, "IDLE": 1, "PAUSED": 3, "FINISHED": 4}
+
+MAXPOWER = 100
+THRESHOLD = 80
+MAXTIME = 20# s
+MAX_LEADERBOARD_ENTRIES = 7
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        uic.loadUi("form.ui", self)
+
+        self.status = STATUS["IDLE"]
+
+        self.maxpower = 0
+        self.threshold = THRESHOLD
+
+        self.scoreBar = ScoreBar()
+        self.scoreBar.setGeometry(0,0,200,800)
+        self.scoreBar.show()
+        self.horizontalLayout_4.addWidget(self.scoreBar)
+        self.scoreBar.update_bar(0)
+
+        #self.powerSlider.setMaximum(MAXPOWER)
+
+        self.clockTimer = QTimer(self)
+        self.max_time = MAXTIME
+        self.time = self.max_time
+        self.clockTimer.setInterval(1000)
+        self.clockTimer.timeout.connect(self.updateClock)
+        self.resetTimer()
+
+        self.powerTimer = QTimer(self)
+        self.powerTimer.setInterval(100)
+        self.powerTimer.timeout.connect(self.updatePowerMeter)
+
+        try:
+            self.leaderBoard = np.load("leaderboard.npy").tolist()
+            self.updateLeaderBoard()
+        except FileNotFoundError:
+            self.leaderBoard = []
+
+    @pyqtSlot()
+    def startButtonPressed(self):
+        if self.status == STATUS["IDLE"] or self.status == STATUS["PAUSED"]:
+            self.startButton.setText("Pause")
+            self.status = STATUS["RUNNING"]
+            self.clockTimer.start()
+            self.powerTimer.start()
+        elif self.status == STATUS["RUNNING"]:
+            self.startButton.setText("Continue")
+            self.status = STATUS["PAUSED"]
+            self.clockTimer.stop()
+
+        if self.status != STATUS["IDLE"]:
+            self.nameInput.setEnabled(False)
+
+    @pyqtSlot()
+    def resetButtonPressed(self):
+        self.startButton.setText("Start")
+        self.startButton.setEnabled(True)
+        self.resetTimer()
+        self.nameInput.setEnabled(True)
+        self.status = STATUS["IDLE"]
+        self.maxpower = 0
+        self.maxPowerSlider.setValue(0)
+
+    def resetTimer(self):
+        self.clockTimer.stop()
+        self.time = self.max_time
+        self.timerLabel.setText(f"{self.time//60:02}:{self.time%60:02}")
+
+    def updateClock(self):
+        self.time -= 1
+        self.timerLabel.setText(f"{self.time//60:02}:{self.time%60:02}")
+        if self.time <= 10:
+            # 10 s left, red timer!
+            self.timerLabel.setStyleSheet('color: red;')
+
+        if self.time <= 0:
+            self.status = STATUS["FINISHED"]
+            self.timesup()
+
+    def timesup(self):
+        self.startButton.setEnabled(False)
+        self.clockTimer.stop()
+        self.updateLeaderBoard()
+
+    def updateLeaderBoard(self):
+        if len(self.nameInput.text()) > 0:
+            self.leaderBoard.append([self.nameInput.text(), self.maxpower, self.time])
+            np.save("leaderboard.npy", self.leaderBoard)
+        lb_arr = np.array(self.leaderBoard)
+        times = lb_arr[:, 2].astype(float)
+        isort = np.argsort(times)
+        lb_arr = lb_arr[isort, :]
+
+        i_timeout = np.where(times[isort] == 0)[0]
+
+        timeout_arr = lb_arr[i_timeout,:]
+
+        isort_timeout = np.argsort(timeout_arr[:, 1].astype(float))
+
+        lb_arr[i_timeout, :] = lb_arr[i_timeout, :][isort_timeout]
+        lb_arr = lb_arr[::-1,:]
+
+        # Now lib_arr has been sorted according to (1) time to finish and (2) max power achieved.
+
+        self.label_3.setText(self.generateLeaderboardTxt(lb_arr))
+
+        print(lb_arr)
+
+    def generateLeaderboardTxt(self, lb_array):
+
+        text = f"{'rank' : <5}{'name' : <20}{'score' : <7}{'time' : <4}\n"
+        for i in range(min(MAX_LEADERBOARD_ENTRIES, len(lb_array))):
+            row = lb_array[i]
+            elapsed = MAXTIME - int(row[2])
+            text += f"{str(i+1)+'.': <5}{row[0] : <20}{float(row[1]): <8.2f}{int(elapsed)//60:02}:{int(elapsed)%60:02}\n"
+        
+        return text
+
+    def updatePowerMeter(self):
+
+        power = self.getCurrentPower()
+
+        #.setValue(int(power))
+
+        self.scoreBar.update_bar(int(power))
+
+        if power > self.maxpower and self.status == STATUS["RUNNING"]:
+            self.maxpower = power
+            self.maxPowerSlider.setValue(int(power))
+            self.powerLabel.setText(str(int(power)))
+
+            if power >= self.threshold:
+                self.status = STATUS["FINISHED"]
+                self.timesup()
+
+    def getCurrentPower(self):
+        # Insert code connecting to power meter here!
+        return np.random.random()*100*(1+self.max_time - self.time)/(self.max_time+1)
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    widget = MainWindow()
+    widget.show()
+    sys.exit(app.exec())
---
+++ scorebar.py
@@ -0,0 +1,80 @@
+# importing libraries
+from PyQt6.QtWidgets import * 
+from PyQt6.QtGui import * 
+from PyQt6.QtCore import * 
+import sys
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+
+class ScoreBar(QProgressBar):
+
+    def __init__(self):
+
+        super(ScoreBar, self).__init__()
+
+        # Creating color map for progress bar
+        self.cmap0 = plt.get_cmap('hot')
+        self.cmap = plt.get_cmap('hot')
+
+        # creating progress bar
+        #self.bar = QProgressBar(self)
+
+        # setting geometry to progress bar
+        #self.setGeometry(0, 0, 200, 800)
+        self.setTextVisible(0)
+
+        # set value to progress bar
+        self.maxval = 100
+        self.setValue(self.maxval)
+
+        # setting alignment to center
+        self.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.setOrientation(Qt.Orientation.Vertical)
+
+        #setting gradient color to progress bar
+        self.cmapmax = .75#.99
+        aa = str(mpl.colors.to_hex(self.cmap(self.cmapmax)))
+        ab = str(mpl.colors.to_hex(self.cmap(self.cmapmax/2)))
+
+        self.setStyleSheet("QProgressBar::chunk "
+                    "{"
+                    "background: QLinearGradient( x1: 0, y1: 0,"
+                                                "x2: 0, y2: 1,"
+                                            "stop: 0 "+aa+","
+                                            "stop: 1 "+ab+" );"
+                    "}"
+                    "QProgressBar "
+                    "{"
+                    "border : none;"
+                #   "background: QLinearGradient( x1: 0, y1: 0,"
+                #                                 "x2: 1, y2: 0,"
+                #                             "stop: 0 #00ffff,"
+                #                             "stop: 1 #ff000f );"
+                    "}")
+
+
+    def update_bar(self, value):
+
+        # set value to progress bar
+        self.setValue(value)
+
+        #setting gradient color to progress bar
+        cmapval = self.cmapmax*(value/self.maxval)
+        aa = str(mpl.colors.to_hex(self.cmap(cmapval)))
+        ab = str(mpl.colors.to_hex(self.cmap(cmapval/2)))
+        self.setStyleSheet("QProgressBar::chunk "
+                    "{"
+                    "background: QLinearGradient( x1: 0, y1: 0,"
+                                                "x2: 0, y2: 1,"
+                                            "stop: 0 "+aa+","
+                                            "stop: 1 "+ab+" );"
+                    "}"
+                    "QProgressBar "
+                    "{"
+                    "border : none;"
+                #   "background: QLinearGradient( x1: 0, y1: 0,"
+                #                                 "x2: 1, y2: 0,"
+                #                             "stop: 0 #00ffff,"
+                #                             "stop: 1 #ff000f );"
+                    "}")
